e800a3877523225dc955b74a272b97c24d9da961
Daniel Pascual <ek791957@ens.uqam.ca>
2023-09-28 10:01:39 -0400

    creation de component new-message-form

--- frontend/src/app/new-message-form/new-message-form.component.ts
+++ frontend/src/app/new-message-form/new-message-form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MessagesService } from '../chat/messages.service';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from '../login/authentication.service';
 
 @Component({
   selector: 'app-new-message-form',
@@ -6,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-message-form.component.css']
 })
 export class NewMessageFormComponent {
+  username$ = this.authenticationService.getUsername();
 
+  messageForm = this.fb.group({
+    msg: "",
+  });
+
+  username: string | null = null;
+  usernameSubscription: Subscription;
+
+  constructor(
+    private fb: FormBuilder,
+    private messagesService: MessagesService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.usernameSubscription = this.username$.subscribe((u) => {
+      this.username = u;
+    });
+  }
+
+  onPublishMessage() {
+    if (this.username && this.messageForm.valid) { 
+      this.messagesService.postMessage({
+        text: this.messageForm.value.msg as string,
+        username: this.username,
+        timestamp: Date.now(),
+      });
+      this.messageForm.reset();
+    }
+  }
 }
