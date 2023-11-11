188c3f2172c7057d80fb875cc354b85bfd328d75
Daniel Pascual <ek791957@ens.uqam.ca>
2023-11-10 21:20:51 -0500

    update get&create message for Firebase

--- backend/src/main/java/com/inf5190/chat/messages/repository/MessageRepository.java
+++ backend/src/main/java/com/inf5190/chat/messages/repository/MessageRepository.java
@@ -1,10 +1,17 @@
 package com.inf5190.chat.messages.repository;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.ExecutionException;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
 import com.inf5190.chat.messages.model.Message;
 
 import org.springframework.stereotype.Repository;
@@ -17,24 +24,56 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class MessageRepository {
     private final List<Message> messages = new ArrayList<Message>();
-    private final AtomicLong idGenerator = new AtomicLong(0);
 
-    public List<Message> getMessages(Long fromId) {
-        List<Message> messages = this.messages.stream().sorted(Comparator.comparingLong((m) -> m.timestamp())).toList();
-        if (fromId == null) {
-            return messages;
+    ///////////////
+    private static final String COLLECTION_NAME = "messages";
+    private final Firestore firestore = FirestoreClient.getFirestore();
+    // Collection de userAccount
+    final CollectionReference collectionRef = firestore.collection(
+            COLLECTION_NAME);
+
+    //////////////
+
+    public List<Message> getMessages(String fromId) throws InterruptedException, ExecutionException {
+        // Pour faire une requêtre, on utilise des Query
+        // Ici, on ordonne par timestamp
+        // Attention, chaque appel orderBy et limitToLast retourne une nouvelle Query!
+        // Ce n'est pas un builder pattern qui modifie la query précédente.
+        Query query = null;// lastTwoqueries
+
+        if (fromId == null)
+            query = collectionRef.orderBy("timestamp").limit(20);
+        else {
+            DocumentSnapshot doc3 = collectionRef.document(fromId).get().get();
+            query = collectionRef.orderBy("timestamp").startAfter(doc3);
         }
 
-        return messages.stream().dropWhile((m) -> m.id() != fromId).skip(1).toList();
+        // Pour exécuter la query on applle get.
+        ApiFuture<QuerySnapshot> entries = query.get();// lastTwoentries
+        // On attend la complétion.
+        QuerySnapshot querySnapshot = entries.get();
+
+        // Conversion du resultat en object FirestoreMessage
+        List<Message> result = new ArrayList<>();
+
+        querySnapshot.forEach(document -> {
+            String id = document.getId();
+            FirestoreMessage m = document.toObject(FirestoreMessage.class);
+            result.add(new Message(id, m.getUsername(), m.getTimestamp().getSeconds(), m.getText()));
+        });
+        // System.out.println(result);
+        return result;
     }
 
     public Message createMessage(Message message) {
-        Message newMessage = new Message(this.idGenerator.incrementAndGet(), message.username(),
-                System.currentTimeMillis(),
-                message.text());
+        FirestoreMessage firemsg = new FirestoreMessage(message.username(), Timestamp.now(), message.text());
+        try {
+            firestore.collection(COLLECTION_NAME).document().create(firemsg);
+        } catch (Exception e) {
+            throw new UnsupportedOperationException(e);
+        }
 
-        this.messages.add(newMessage);
-        return newMessage;
+        return message;
     }
 
 }
