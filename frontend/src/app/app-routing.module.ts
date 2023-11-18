080e35ae3dac88d49e6711110d74d5e0ca269906
Daniel Pascual <ek791957@ens.uqam.ca>
2023-11-17 20:56:38 -0500

    adding Guard router logic

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LoginPageComponent } from "./login/login-page/login-page.component";
 import { ChatPageComponent } from "./chat/chat-page/chat-page.component";
+import { loginPageGuard } from "./guards/login-page.guard";
 
 const routes: Routes = [
   { path: "chat", component: ChatPageComponent },
-  { path: "**", component: LoginPageComponent },
+  { path: "**", component: LoginPageComponent, canActivate: [loginPageGuard] },
 ];
 
 @NgModule({
--- frontend/src/app/guards/login-page.guard.ts
+++ frontend/src/app/guards/login-page.guard.ts
@@ -1,5 +1,10 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 export const loginPageGuard: CanActivateFn = (route, state) => {
-  return true;
+  if (localStorage.getItem("username") === null) {
+    return true; 
+  } else {
+    return inject(Router).parseUrl("/chat");
+  }
 };
