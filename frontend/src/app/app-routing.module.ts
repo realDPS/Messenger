ba02dc609d1b34bc310cedddc8d7976dbd327b88
Daniel Pascual <ek791957@ens.uqam.ca>
2023-11-17 21:10:05 -0500

    add chat page guard redirection

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from "@angular/router";
 import { LoginPageComponent } from "./login/login-page/login-page.component";
 import { ChatPageComponent } from "./chat/chat-page/chat-page.component";
 import { loginPageGuard } from "./guards/login-page.guard";
+import { chatPageGuard } from "./guards/chat-page.guard";
 
 const routes: Routes = [
-  { path: "chat", component: ChatPageComponent },
+  { path: "chat", component: ChatPageComponent,canActivate: [chatPageGuard] },
   { path: "**", component: LoginPageComponent, canActivate: [loginPageGuard] },
 ];
 
