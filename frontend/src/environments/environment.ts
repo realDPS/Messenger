62293c435bd16c11b1b422bd9fc24bde7cbc2f1f
Daniel Pascual <ek791957@ens.uqam.ca>
2023-12-15 21:30:49 -0500

    adding wss

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   backendUrl: "https://chat-app-6aotv6tyuq-nn.a.run.app",
-  wsUrl: "ws://chat-app-6aotv6tyuq-nn.a.run.app",
+  wsUrl: "wss://chat-app-6aotv6tyuq-nn.a.run.app",
 };
