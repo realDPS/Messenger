c0d8db555a85f90efc81061b14931cfa24e10858
Omar Zoui <omarzoui1996@hotmail.com>
2023-12-18 17:51:20 -0500

    env et nom d'equipe

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  backendUrl: "http://localhost:8080",
-  wsUrl: "ws://localhost:8080",
+  backendUrl: "https://chat-app-6aotv6tyuq-nn.a.run.app",
+  wsUrl: "wss://chat-app-6aotv6tyuq-nn.a.run.app",
 };
